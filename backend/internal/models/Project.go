2603456d9074b762a5c13a6fc86164a68d952971
MuhammadFelda <ifelfelda27@gmail.com>
2026-03-13 19:13:54 +0700

    Added models project

--- backend/internal/models/Project.go
+++ backend/internal/models/Project.go
@@ -1,17 +1,15 @@
 package models
 
-import "time"
+import "gorm.io/gorm"
 
 type Project struct {
-	Id				uint		`json:"id"`
-	Name			string		`json:"name"`
+	gorm.Model
+	Name			string		`gorm:"not null" json:"name"`
 	ProjectOwnerId	uint		`json:"project_owner_id"`
 	Creator			uint		`json:"creator"`
 	Updater			uint		`json:"updater"`
-	IsDeleted		bool		`json:"is_deleted"`
-	CreatedAt		time.Time	`json:"created_at"`
-	UpdatedAt		time.Time	`json:"updated_at"`
+	IsDeleted		bool		`gorm:"not null" default:"false" json:"is_deleted"`
 
-	ProjectOwner	*ProjectOwner	`json:"project_owner,omitempty"`
-	Task			[]Task			`json:"task,omitempty"`
+	ProjectOwner	*ProjectOwner	`gorm:"foreignKey:ProjectOwnerId" json:"project_owner,omitempty"`
+	Task			[]Task			`gorm:"foreignKey:ProjectId" json:"tasks,omitempty"`
 }
