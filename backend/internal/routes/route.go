916949bb080e7be782131fb19bc71a700ec2140c
MuhammadFelda <ifelfelda27@gmail.com>
2026-03-13 19:13:20 +0700

    Added route for APIs endpoint

--- backend/internal/routes/route.go
+++ backend/internal/routes/route.go
@@ -5,24 +5,32 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func Register(r *gin.Engine) {
+func Register(
+	r *gin.Engine,
+	logtimeCtrl *controllers.LogtimeController,
+	projectOwnerCtrl *controllers.ProjectOwnerController,
+	skillCtrl *controllers.SkillController,
+	userCtrl *controllers.UserController,
+) {
 	api := r.Group("/api")
 
-	logtime := controllers.NewLogtimeController()
-	api.GET("/logtimes", logtime.Index)
-	api.GET("/logtimes/:id", logtime.Show)
-	api.POST("/logtimes", logtime.Create)
-	api.DELETE("/logtimes/:id", logtime.Delete)
+	api.GET("/logtimes", logtimeCtrl.Index)
+	api.GET("/logtimes/:id", logtimeCtrl.Show)
+	api.POST("/logtimes", logtimeCtrl.Create)
+	api.DELETE("/logtimes/:id", logtimeCtrl.Delete)
 
-	skill := controllers.NewSkillController()
-	api.GET("/skills", skill.Index)
-	api.GET("/skills/:id", skill.Show)
-	api.POST("/skills", skill.Create)
-	api.DELETE("/skills/:id", skill.Delete)
+	api.GET("/skills", skillCtrl.Index)
+	api.GET("/skills/:id", skillCtrl.Show)
+	api.POST("/skills", skillCtrl.Create)
+	api.DELETE("/skills/:id", skillCtrl.Delete)
 
-	projectOwner := controllers.NewProjectOwnerController()
-	api.GET("/projectOwners", projectOwner.Index)
-	api.POST("/projectOwners/", projectOwner.Create)
-	api.PUT("/projectOwners/:id", projectOwner.Update)
-	api.DELETE("/projectOwners/:id", projectOwner.Delete)
+	api.GET("/projectOwners", projectOwnerCtrl.Index)
+	api.POST("/projectOwners/", projectOwnerCtrl.Create)
+	api.PUT("/projectOwners/:id", projectOwnerCtrl.Update)
+	api.DELETE("/projectOwners/:id", projectOwnerCtrl.Delete)
+
+	api.GET("/users", userCtrl.Index)
+	api.POST("/users", userCtrl.Create)
+	api.PUT("/users/:id", userCtrl.Update)
+	api.DELETE("/users/:id", userCtrl.Delete)
 }
