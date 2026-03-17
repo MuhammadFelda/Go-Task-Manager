37a987db292544ad2ac5476bef5ca6dedfe5c4fd
MuhammadFelda <ifelfelda27@gmail.com>
2026-03-17 15:23:43 +0700

    Added model for Log

--- backend/internal/models/Log.go
+++ backend/internal/models/Log.go
@@ -1,5 +1,10 @@
 package models
 
+import "gorm.io/gorm"
+
 type Log struct {
-	
+	gorm.Model
+	UserId			uint		`gorm:"not null" json:"user_id"`
+	Target			string		`json:"target"`
+	Description		string		`json:"description"`
 }
