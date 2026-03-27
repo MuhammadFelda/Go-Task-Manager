7a77f19e5285bcecc844c93721ad715d589b2936
MuhammadFelda <ifelfelda27@gmail.com>
2026-03-27 16:46:01 +0700

    feat: added handle CORS header

--- backend/cmd/main/main.go
+++ backend/cmd/main/main.go
@@ -47,6 +47,17 @@ func main() {
 	r := gin.Default()
 	r.SetTrustedProxies(nil)
 
+	r.Use(func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+		ctx.Header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
+		ctx.Header("Access-Control-Allow-Headers", "Content-Type")
+		if ctx.Request.Method == "OPTIONS" {
+			ctx.AbortWithStatus(204)
+			return
+		}
+		ctx.Next()
+	})
+
 	routes.Register(r, logtimeController, projectOwnerController, skillController, userController)
 
 	port := os.Getenv("APP_PORT")
